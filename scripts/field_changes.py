b6a3a67c304a2477a05d128b7fce3000ad84edd3
Matthew Lemon <lemon@matthewlemon.com>
2017-06-06 10:55:51 +0100

    rough script to amend datamap file based on finance_changes.csv file

--- scripts/field_changes.py
+++ scripts/field_changes.py
@@ -8,28 +8,41 @@ changes fieldnames in datamap csv file
 """
 
 import csv
-import codecs
 
 
 def get_amending_fields() -> list:
-    with codecs.open(
-            '../finance_changes.csv', 'r', errors='ignore') as amendment_file:
+    with open('../finance_changes.csv', 'r') as amendment_file:
         a_reader = csv.reader(amendment_file)
         return [(item[1], item[0]) for item in a_reader]
 
 
-amending_fields = get_amending_fields()
+if __name__ == "__main__":
 
-with open('../new-dm.csv', 'w', encoding='utf-8') as outfile:
-    writer = csv.writer(outfile)
+    amending_fields = get_amending_fields()
 
-    with codecs.open('../datamap-returns-to-master-WITH_HEADER_FORSQLITE.csv', 'r+', errors='ignore') as f:
-        reader = csv.reader(f)
-        for line in reader:
-            # TODO need to test for membership of a tuple within a list here
-            if line[0] in amending_fields:
-                writer.writerow(
-                    [amending_fields[amending_fields.index(line[0])], line[1], line[2]])
-            else:
-                writer.writerow(
-                    [line[0], line[1], line[2]])
+    with open('../new-dm.csv', 'w', encoding='utf-8') as outfile:
+        writer = csv.writer(outfile)
+
+        with open(
+            '../datamap-returns-to-master-WITH_HEADER_FORSQLITE.csv', 'r+', encoding='latin1') as f:
+            reader = csv.reader(f)
+            for line in reader:
+
+                # get the number of items in the dm line
+                indices = list(range(len(line)))
+
+                # if the key dm is in the list of fields to be changed...
+                if line[0] in [a_item[1] for a_item in amending_fields]:
+                    # put the new key on its own in a list
+                    w_row = [[item[0] for item in amending_fields if
+                              item[1] == line[0]][0]]
+
+                    # if we have more items in the dm line...
+                    if len(indices) > 1:
+                        # make a list of the remaining dm line items
+                        remaining_dm_items = [line[x] for x in indices[1:]]
+                        w_row = w_row + remaining_dm_items
+                    writer.writerow(w_row)
+                else:
+                    w_row = [line[i] for i in indices]
+                    writer.writerow(w_row)
